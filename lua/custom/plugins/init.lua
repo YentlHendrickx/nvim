291e9c8069615d3407e45a01c71b1cdf131d49c9
YentlHendrickx <yentl@kuubix.be>
2024-10-01 11:23:56 +0200

    init

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,54 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    opts = {
+      menu = {
+        width = vim.api.nvim_win_get_width(0) - 4,
+      },
+      settings = {
+        save_on_toggle = true,
+      },
+    },
+    keys = function()
+      local keys = {
+        {
+          '<leader>a',
+          function()
+            require('harpoon'):list():add()
+          end,
+          desc = 'Harpoon File',
+        },
+        {
+          '<C-h>',
+          function()
+            local harpoon = require 'harpoon'
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = 'Harpoon Quick Menu',
+        },
+      }
+
+      local used_keys = {
+        'h',
+        'j',
+        'k',
+        'l',
+      }
+      for i = 1, 4 do
+        table.insert(keys, {
+          '<leader>' .. used_keys[i],
+          function()
+            require('harpoon'):list():select(i)
+          end,
+          desc = 'Harpoon to File ' .. i,
+        })
+      end
+      return keys
+    end,
+  },
+}
